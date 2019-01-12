afbf7da312821d0c4fb0569022f7697ba321d573
Raymond L. Rivera <ray.l.rivera@gmail.com>
2019-01-12 16:15:12 -0700

    validators: Add argument validator function
    Signed-off-by: Raymond L. Rivera <ray.l.rivera@gmail.com>

---
+++ sudoku/validators.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+'''
+'''
+
+import argparse as ap
+
+
+def real_positive_number(s: str) -> float:
+    '''Validates that the input is a positive real number.'''
+    msg = 'value must be a positive real number'
+    try:
+        n = float(s)
+        if n <= 0:
+            raise ap.ArgumentTypeError(msg)
+        return n
+    except ValueError:
+        raise ap.ArgumentTypeError(msg)
